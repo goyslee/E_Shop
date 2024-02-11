efc52534493883afe9a6ba6c41334acda5af78e9
goyslee <goyslee@gmail.com>
2024-02-11 21:52:41 +0000

    App.js-cors-3

--- backend/server.js
+++ backend/server.js
@@ -26,24 +26,37 @@ const port = process.env.PORT;
 const swaggerDocument = YAML.load(fs.readFileSync('./swagger.yaml', 'utf8'));
 
 app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin",
+//     `${process.env.REACT_APP_FRONTEND_URL}`,
+//     `${process.env.REACT_APP_BACKEND_URL}`,
+//     `${process.env.LOCALHOST}`,
+//     `${process.env.REACT_APP_FRONTEND_URL}:${process.env.REACT_APP_LOCAL_PORT}`,
+//     `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_LOCAL_PORT}`,
+//     'https://merchant-ui-api.stripe.com/elements/wallet-config');
+//   res.header("Access-Control-Allow-Methods",
+//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+//   )
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   )
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next()
+// })
+
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin",
-    `${process.env.REACT_APP_FRONTEND_URL}`,
-    `${process.env.REACT_APP_BACKEND_URL}`,
-    `${process.env.LOCALHOST}`,
-    `${process.env.REACT_APP_FRONTEND_URL}:${process.env.REACT_APP_LOCAL_PORT}`,
-    `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_LOCAL_PORT}`,
-    'https://merchant-ui-api.stripe.com/elements/wallet-config');
-  res.header("Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  )
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  )
-  res.header("Access-Control-Allow-Credentials", true);
-  next()
-})
+  const origin = req.headers.origin; 
+
+  // Add a check here to validate 'origin' against a list of trusted origins
+  if (origin && ['https://e-shop-frontend-8ylf.onrender.com', 'https://other-allowed-site.com'].includes(origin)) { 
+    res.header("Access-Control-Allow-Origin", origin); // Reflect the origin back
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  next();
+});
 
 console.log(`${process.env.REACT_APP_FRONTEND_URL}`,
     `${process.env.REACT_APP_BACKEND_URL}`,
